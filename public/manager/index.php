3ce474b0b8bee61d800f764a8f42cccda1f0a947
mauro.larese <mauro.larese@mm-one.com>
2019-08-29 18:46:05 +0200

    update

--- public/manager/index.php
+++ public/manager/index.php
@@ -6,3 +6,31 @@
  * Time: 18:44
  */
 echo "index";
+?>
+$db = new \MicroDB\Database('data/posts'); // data directory
+
+// create an item
+// id is an auto incrementing integer
+$id = $db->create(array(
+'title' => 'Lorem ipsum',
+'body' => 'At vero eos et accusam et justo duo dolores et ea rebum.'
+));
+
+// load an item
+$post = $db->load($id);
+
+// save an item
+$post['tags'] = array('lorem', 'ipsum');
+$db->save($id, $post);
+
+// find items
+$posts = $db->find(function($post) {
+return is_array(@$post['tags']) && in_array('ipsum', @$post['tags']);
+});
+
+foreach($posts as $id => $post) {
+print_r($post);
+}
+
+// delete an item
+$db->delete($id);
